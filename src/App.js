97e336efd88cfac5b18bd0f0fa2365946ed46ec7
Muhammad Saad Virk <m.saad.virk.12@gmail.com>
2022-12-20 00:19:48 +0500

    Footer and ScrollUp component added with change of header navgation menu buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
+import ScrollUp from './components/scrollup/ScrollUp';
 import Skills from './components/skills/Skills';
 
 function App() {
@@ -17,7 +19,11 @@ function App() {
           <Skills />
           <Qualification />
           <Contact />
+          
         </main>
+
+        <Footer />
+        <ScrollUp />
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 import './header.css'
-import { UilTimes, UilApps, UilEstate, UilUser, UilFileAlt, UilBriefcaseAlt, UilScenery, UilMessage } from '@iconscout/react-unicons'
+import { UilTimes, UilApps, UilEstate, UilUser, UilFileAlt, UilBriefcaseAlt, UilMessage } from '@iconscout/react-unicons'
 
 const Header = () => {
     /* ============ TOGGLE MENU ==============    */
+    window.addEventListener("scroll", function () {
+        const header = this.document.querySelector(".header");
+        if (this.scrollY >= 80) header.classList.add("scroll-header")
+        else header.classList.remove("scroll-header")
+    })
+
+    /* ============ TOGGLE MENU ==============    */
+    const [activeNav, setActiveNav] = useState("#home");
     const [toggle, showMenu] = useState(false);
 
   return (
@@ -14,25 +22,25 @@ const Header = () => {
             <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
                 <ul className="nav__list grid">
                     <li className="nav__item">
-                        <a href="#home" className="nav__link active-link">
+                        <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? "nav__link active-link" : "nav__link"}>
                             <UilEstate className='nav__icon' /> Home
                         </a>
                     </li>
 
                     <li className="nav__item">
-                        <a href="#about" className="nav__link">
+                        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? "nav__link active-link" : "nav__link"}>
                             <UilUser className='nav__icon' /> About
                         </a>
                     </li>
 
                     <li className="nav__item">
-                        <a href="#skills" className="nav__link">
+                        <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? "nav__link active-link" : "nav__link"}>
                             <UilFileAlt className='nav__icon' /> Skills
                         </a>
                     </li>
 
                     <li className="nav__item">
-                        <a href="#qualification" className="nav__link">
+                        <a href="#qualification" onClick={() => setActiveNav('#qualification')} className={activeNav === '#qualification' ? "nav__link active-link" : "nav__link"}>
                             <UilBriefcaseAlt className='nav__icon' /> Qualification
                         </a>
                     </li>
@@ -44,7 +52,7 @@ const Header = () => {
                     </li> */}
                     
                     <li className="nav__item">
-                        <a href="#contact" className="nav__link">
+                        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? "nav__link active-link" : "nav__link"}>
                             <UilMessage className='nav__icon' /> Contact
                         </a>
                     </li>
